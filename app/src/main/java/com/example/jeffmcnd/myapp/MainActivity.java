06e5d5b90f6a2a15cdf24a7ccaf258840f0a35cb
Jeffrey McNally-Dawes <jeffmcnallydawes@gmail.com>
2016-09-21 18:49:15 -0700

    add in drawer navigation
    This commit adds in a drawer for navigation using a few default, planned
    values. The drawer does not do anything at this point.

--- app/src/main/java/com/example/jeffmcnd/myapp/MainActivity.java
+++ app/src/main/java/com/example/jeffmcnd/myapp/MainActivity.java
@@ -1,37 +1,55 @@
 package com.example.jeffmcnd.myapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.jeffmcnd.myapp.activity.BrewerActivity;
 import com.example.jeffmcnd.myapp.fragments.BeverageFragment;
 import com.example.jeffmcnd.myapp.fragments.BrewerFragment;
 import com.example.jeffmcnd.myapp.models.Beverage;
 import com.example.jeffmcnd.myapp.models.Brewer;
-import com.f2prateek.dart.Dart;
-import com.f2prateek.dart.HensonNavigable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity
         extends FragmentActivity
         implements BrewerFragment.OnListFragmentInteractionListener,
         BeverageFragment.OnListFragmentInteractionListener {
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.drawer_lv) ListView drawerListView;
 
-    MyFragmentPagerAdapter mPagerAdapter;
-    ViewPager mViewPager;
+    private String[] drawerOptions;
+    private MyFragmentPagerAdapter mPagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        drawerOptions = getResources().getStringArray(R.array.drawer_options);
+
+        drawerListView.setAdapter(new DrawerListAdapter(this, new ArrayList<String>(Arrays.asList(drawerOptions))));
 
         mPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.pager);
@@ -86,6 +104,59 @@ class MyFragmentPagerAdapter extends FragmentPagerAdapter {
                 return "Beverages";
             default:
                 return "Brewers";
+        }
+    }
+}
+
+class DrawerListAdapter extends BaseAdapter {
+    Context context;
+    ArrayList<String> items;
+
+    public DrawerListAdapter(Context context, ArrayList<String> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (view != null) {
+            holder = (ViewHolder) view.getTag();
+        } else {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            view = inflater.inflate(R.layout.drawer_list_item, parent, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        }
+
+        String name = (String) getItem(position);
+
+        holder.nameTextView.setText(name);
+
+        return view;
+    }
+
+    static class ViewHolder {
+        @BindView(R.id.drawer_item_name) TextView nameTextView;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
         }
     }
 }
